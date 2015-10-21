0c543d2aaff5bd5c8aa0912b1561fa40ff943cc6
Sho Kusano <rosylilly@aduca.org>
2015-10-21 12:03:48 +0900

    Change generate job environment. generator in the docker.

--- app/jobs/shard/doc/generate_job.rb
+++ app/jobs/shard/doc/generate_job.rb
@@ -30,16 +30,14 @@ class Shard::Doc::GenerateJob < ActiveJob::Base
 
     sleep 2
 
-    log("Clone #{@doc.shard.full_name}")
     FileUtils.mkdir_p(working_dir)
 
-    progress_lambda = lambda do |msg|
-      log(msg, plain: false)
-    end
+    log("Create container for #{@doc.shard.full_name}")
+    container.start({ 'Binds' => ["#{working_dir}:#{container_doc_path}"] })
 
-    @repository = Rugged::Repository.clone_at(@doc.shard.git_url, working_dir.to_s, progress: progress_lambda)
-    @repository.checkout_tree(@doc.sha, strategy: :force)
-    log("Checkout : #{@doc.sha}")
+    log("Clone #{@doc.shard.full_name}")
+    shell('git', 'clone', @doc.shard.git_url, container_working_dir)
+    shell('git', 'checkout', @doc.sha, chdir: container_working_dir)
 
     generate_document
     upload_document
@@ -55,6 +53,9 @@ class Shard::Doc::GenerateJob < ActiveJob::Base
     pusher_threads.all_waits
 
     if @doc
+      container.kill
+      container.wait
+      container.delete
       FileUtils.rm_rf(working_dir) if File.directory?(working_dir)
       Docrystal.redis.del(@doc.log_redis_key)
     end
@@ -68,6 +69,26 @@ class Shard::Doc::GenerateJob < ActiveJob::Base
     )
   end
 
+  def container
+    @container ||= Docker::Container.create(
+      'Image' => 'docrystal/crystal:v0.9.0',
+      'Cmd' => ['bash', '-c', 'while : ; do sleep 1; done'],
+      'Tty' => false,
+      'OpenStdin' => false,
+      'Volumes' => {
+        container_doc_path => {}
+      }
+    )
+  end
+
+  def container_working_dir
+    @container_working_dir ||= Pathname.new('/tmp/src').join(@doc.shard.full_name)
+  end
+
+  def container_doc_path
+    '/tmp/artifacts'
+  end
+
   def generate_document
     execute_pre_commands
     execute_doc_commands
@@ -75,24 +96,23 @@ class Shard::Doc::GenerateJob < ActiveJob::Base
   end
 
   def execute_pre_commands
-    shell('env')
-
     if File.exists?(working_dir.join('shard.yml'))
-      shell('shards', 'install')
+      shell('shards', 'install', chdir: container_working_dir)
     else
-      shell('crystal', 'deps') if File.exists?(working_dir.join('Projectfile'))
+      shell('crystal', 'deps', chdir: container_working_dir) if File.exists?(working_dir.join('Projectfile'))
     end
   end
 
   def execute_doc_commands
     if @doc.shard.full_name == 'github.com/manastech/crystal'
-      shell('make', 'doc')
+      shell('make', 'doc', chdir: container_working_dir)
     else
-      shell('crystal', 'doc')
+      shell('crystal', 'doc', chdir: container_working_dir)
     end
   end
 
   def execute_post_commands
+    shell('cp', '-r', container_working_dir.join('doc'), container_doc_path)
   end
 
   def upload_document
@@ -113,31 +133,29 @@ class Shard::Doc::GenerateJob < ActiveJob::Base
     log("\nAll File uploaded")
   end
 
-  def shell(cmd, *args)
+  def shell(cmd, *args, chdir: nil)
     log("$ #{cmd} #{args.join(' ')}")
 
     cmd_log = ["$ #{cmd} #{args.join(' ')}\n"]
 
-    retval = Open3.popen3(environments, cmd, *args, chdir: working_dir) do |input, stdout, stderr, wait|
-      input.close
-
-      stdout.each do |line|
-        log(line, plain: false)
-        cmd_log << line
-      end
-
-      stderr.each do |line|
-        logger.error(line)
-        cmd_log << line
-      end
-
-      wait.value
+    command = ['bash', '-c']
+    if chdir
+      command += ["cd #{chdir} && #{cmd} #{args.join(' ')}"]
+    else
+      command += ["#{cmd} #{args.join(' ')}"]
     end
 
-    fail ShellError, cmd_log.join("") unless retval.success?
+    retval = container.exec(command) do |stream, chunk|
+      log(chunk, plain: false)
+      cmd_log << chunk.to_s
+    end
+
+    fail ShellError, cmd_log.join("") unless retval.last == 0
   end
 
   def log(msg, plain: true)
+    return if msg.nil? || msg.empty?
+
     logger.info(msg.strip)
 
     if @doc
